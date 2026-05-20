bf4c2586ff1978e715cf38902b2ba66237bc32be
Marcos <markaragonromero@gmail.com>
2026-05-20 12:29:13 +0200

    Keys securizadas

--- backend/app/keys/key_gen.py
+++ backend/app/keys/key_gen.py
@@ -5,11 +5,3 @@ import secrets
 
 
 print(secrets.token_hex(32))
-
-
-# Key almacenada para auth_tokens.py
-key="3a5878ad1068da64a065f1f68b00a17aa2c4ad33a84effe1c5ad499ef2c7dda5"
-
-
-# Key usada para la contraseña de la base de datos en el docker-compose.yml
-db_key="2b295fed2ac875775c825fbe2ba996842d09ebc3381cf148b81a68a7a0236373"
